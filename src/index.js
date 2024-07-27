6cef1838ebe880180ee195210758556b7244630d
Younghyun Lee <younghyunlee22@gmail.com>
2024-07-27 16:18:56 -0500

    break the test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 // Computes a factorial of a given positive integer
 
 function factorial(n) {
-  if (n < 0) {
-    throw new Error("Factorial is only defined for non-negative integers!");
-  }
+  // if (n < 0) {
+  //   throw new Error("Factorial is only defined for non-negative integers!");
+  // }
 
   if (n === 0) {
     return 1;
